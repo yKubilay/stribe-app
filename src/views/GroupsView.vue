0505de6f407763e560413b068389933251d598f4
yKubilay <kubioz@icloud.com>
2023-03-10 11:18:50 +0100

    Introducing groups as cards + animation, searchbar

--- src/views/GroupsView.vue
+++ src/views/GroupsView.vue
@@ -1,86 +1,350 @@
 <template>
+  <Navigation />
   
-
-  
-<!--   <svg class="svg" viewBox="-50 -80 175 300" xmlns="http://www.w3.org/2000/svg">
-      <path fill="#2E728D" d="M35.9,-45.4C50.5,-46.5,68.9,-43.2,72.5,-33.6C76.1,-24,64.9,-8.1,62.5,9C60.1,26.2,66.5,44.7,61.8,57.7C57,70.6,41,78,25.9,77.6C10.8,77.1,-3.5,68.8,-14.6,60.5C-25.7,52.3,-33.6,44.1,-41.2,35.6C-48.8,27,-56.1,18.1,-59.2,7.5C-62.2,-3,-61,-15.1,-52.9,-20.5C-44.8,-25.9,-29.8,-24.5,-19.8,-25.4C-9.9,-26.4,-4.9,-29.8,2.9,-34.2C10.7,-38.7,21.4,-44.3,35.9,-45.4Z" transform="translate(100 100)" />
-    </svg>
- -->
- 
-
-
-    <Navigation/>
-
-
-
-
   <div class="container">
-  <div
-   v-for = "i in 6"
-   class="card">
-   <header class="card-header">
-    <p class="card-header-title">
-      Coding
-    </p>
+    <header class="groupsHeader" :class="{sticky: stickyHeader}">
+
+    <h1 class="activeGroupsButton">Currently {{ activeGroupsCount }} Groups with {{ activeParticipants }} participants</h1>
+      <input type="text" v-model="searchQuery" placeholder="Search for groups, your interests or other users!" />
   </header>
-  <div class="card-content">
-    <div class="content">
-      <p>Learn Vue with Firebase and state management.....</p>
-      <a href="#"></a>. <a href="#"></a> <a href="#"></a>
-      <br>
-      <time datetime="2016-1-1"></time>
-    </div>
+
+    <section class="basic-grid">
+      <div class="card create-group-card" :style="formStyle" @click="toggleForm">
+        <form :class="{form: showForm}"></form>
+        <h2 class="createGroup" v-if="!showForm">Create group</h2>
+        <div class="plusSymbol" v-if="!showForm">+</div>
+        <div class="formContainer" v-else>
+       
+        <div class="buttonGroup">
+          <button class="floorplanButton">Create</button>
+          <button class="floorplanButton" @click="toggleForm">Cancel</button>
+        </div>
+        </div>
+      </div>
+
+    
+      <div class="card" v-for="(card, index) of cards" :key="card" :style="getCardStyle(index)">
+        <div class="cardContent">
+          
+        <div class="cardTitle">Group {{ card }}</div>
+        
+          <div class="cardDescription">
+        Short description of group
+        </div>
+       <div class="cardParticipants" v-if="isCardExpanded">
+        <h4>Participants</h4>
+        <ul class="participantsList">
+        <li v-for="(participants, index) in participants" :key="index">{{ participants }}</li>  
+        </ul>
+       </div>
+        </div>
+
+        <div class="buttonGroup">
+            <button class="floorplanButton" @click="toggleCard">Show more</button>
+            <button class="floorplanButton" @click="joinRoom">Join room</button>
+          </div>
+      </div>
+    </section>
   </div>
-  <footer class="card-footer">
-    <a href="#" class="card-footer-item">View floorplan</a>
-    <a href="#" class="card-footer-item">Join group</a>
-<!--     <a href="#" class="card-footer-item">Delete</a>
- -->  </footer>
-</div>
-</div>
-
-
 </template>
 
 <script setup>
-    import Navigation from "@/components/Navigation.vue"   
-    import { onMounted, ref } from "vue";
-    import { getAuth, onAuthStateChanged} from "firebase/auth";
+  import Navigation from "@/components/Navigation.vue";
+  import { onMounted, ref, computed } from "vue";
+  import { getAuth, onAuthStateChanged } from "firebase/auth";
+  import { useUserStore } from '@/stores/user.js';
 
-    const isLoggedIn = ref(false); 
+  const storeUser = useUserStore();
 
-let auth;
-onMounted (() => {
-  auth = getAuth();
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      isLoggedIn.value = true;
-    } else {
-      isLoggedIn.value = false;
-    }
-  });
+
+  const isLoggedIn = ref(false);
+  let auth;
+
+onMounted(() => {
+auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+if (user) {
+isLoggedIn.value = true;
+} else {
+isLoggedIn.value = false;
+}
+});
 });
 
-</script>  
+const cards = ref([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
 
+const showForm = ref(false);
+const groupTitle = ref('');
+const groupDescription = ref('');
+const searchQuery = ref('');
+const participants = ref(["jack", "johnny", "joe", "jim", "jack", "johnny"])
+const isCardExpanded = ref(false);
+
+
+
+
+function toggleForm() {
+  if (showForm.value === false) {
+    showForm.value = true;
+  } else {
+    showForm.value = true;
+  }
+}
+
+function toggleCard() {
+  if (isCardExpanded.value === false) {
+    isCardExpanded.value = true;
+  } else {
+    isCardExpanded.value = false;
+  }
+}
+
+
+
+function viewOnFloorplan() {
+
+}
+
+function joinRoom() {
+  if (!participants.value.includes(username.value)) {
+    participants.value.push(username.value);
+  } else {
+    alert("You are already in a group!");
+  }
+}
+
+
+
+/* function removeItem(toRemove) {
+  cards.value = cards.value.filter((card) => card !== toRemove);
+
+} */
+
+const activeGroupsCount = computed(() => cards.value.length);
+const activeParticipants = computed(() => participants.value.length);
+
+
+
+
+  function getCardStyle(index) {
+    const delay = index * 100;
+    return {
+      animationDelay: `${delay}ms`,
+      transition: "transform 0.5s",
+    };
+  }
+
+    const stickyHeader = ref(false);
+
+    window.addEventListener('scroll', () => {
+      if (window.pageYOffset >= 100) { 
+        stickyHeader.value = true;
+      } else {
+        stickyHeader.value = false;
+      }
+    });
+
+ const username = computed(() => {
+  return storeUser.username
+})
+
+
+</script>
 <style>
+  .activeGroupsButton {
+    margin-bottom: 1%;
+  }
 
-.card-footer {
-  display: flex;
-  justify-content: space-between;
+
+
+  .createGroup {
+    font-size: 45px;  
+    color: #edf6f9;   
+  }
+
+  .groupsHeader {
+    background-color: #F5F0E7;
+  
+  }
+
+  .create-group-card {
+  text-align: center;
 }
 
-.card-header {
-  display: flex;
-  background: linear-gradient(15deg, #13547a 0%, #80d0c7 100%);
+  .formContainer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+  }
+
+
+  
+
+
+ .card:hover {
+  transform: scale(1.05);
 }
 
-.container {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
+.expanded {
+  height: auto;
+}
+  input[type="text"], textarea {
+    width: 100%;
+    padding: 8px;
+    font-size: 16px;
+  }
+
+  .basic-grid {
+    display: grid;
+    gap: 1rem;
+    margin-bottom: 5%;
+    margin-top: 2%;
+    grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+
+  }
+
+  .buttonGroup {
+    display: flex;
+    justify-content: center;
+    margin-top: 5%;
+  }
+  
+  .floorplanButton {
+    font-size: 18px;
+    padding:5px;
+/*     background: #2F728D;
+ */    background: #008080;
+ 
+  }
+
+  .plusSymbol {
+    font-size: 80px;
+     
+  }
+ 
+
+
+
+  .card {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background: #353e57;
+    font-size: 3rem;
+    color: white;
+    box-shadow: rgba(3, 8, 20, 0.1) 0px 0.15rem 0.5rem,
+      rgba(2, 8, 20, 0.1) 0px 0.075rem 0.175rem;
+    height: 300px;
+    width: 100%;
+    border-radius: 4px;
+    transition: all 500ms;
+    overflow: hidden;
+    position:relative;
+
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    opacity: 0;
+    transform: translate(-10%, -10%);
+  }
+
+  .card:hover {
+  box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.15);
+  cursor: pointer;
+  transform: translateY(-10px);
+}
+
+.cardContent {
+    text-align: left;
+}
+
+  .cardTitle {
+   font-size: 2rem;
+   color: #edf6f9;   
+   text-align: left;
+
+}
+
+
+  .cardDescription {
+    font-size: 1.3rem;
+    color: #edf6f9;    /*    margin-bottom: 1rem;
+    margin-bottom: 25%; */
+    text-align: left;
+    margin-top: 20%;
+
+  }
+
+  .cardParticipants {
+    list-style-type: none;
+    font-size: 1.3rem;
+
+  }
+
+  .participantsList {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: left;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  .participantsList li {
+    flex: 1 0 calc(50% - 16px);
+    max-width: calc(50% - 16px);
+    margin: 8px;
+    text-align: center;
+  }
+
+
+
+  .card:hover {
+    box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.15);
+    cursor: pointer;
+    
   }
 
   
 
+  .fixed-card {
+   /*    background: linear-gradient(
+      90deg,
+      rgba(2, 0, 36, 1) 0%,
+      rgba(9, 9, 121, 1) 0%,
+      rgba(0, 212, 255, 1) 100%
+    );  */ 
+    background: linear-gradient(15deg, #13547a, #80d0c7); /* primary color */
+
+  }
+
+  /* animate cards */
+  @keyframes cardAnimation {
+    from {
+      opacity: 0;
+      transform: translate(-10%, -10%);
+    }
+    to {
+      opacity: 1;
+      transform: translate(0, 0);
+    }
+  }
+
+  .card:nth-child(odd) {
+    animation-name: cardAnimation;
+    animation-duration: 0.5s;
+    animation-delay: 0ms;
+    animation-fill-mode: forwards;
+  }
+
+  .card:nth-child(even) {
+    animation-name: cardAnimation;
+    animation-duration: 0.5s;
+    animation-delay: 0ms;
+    animation-fill-mode: forwards;
+  }
 </style>
