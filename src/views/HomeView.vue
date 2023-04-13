8a44e0ba3d56a7046bb6d67d758327989e171fee
Kasper Iversen <kasperi@hiof.no>
2023-04-13 12:42:14 +0200

    Worked more on responsiveness

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,7 +18,7 @@
   <div class="row">
     <div class="col-1">
         <h2>Welcome to Stribe</h2>  
-        <h3>Meet and collaborate with <br>likeminded students!</h3>  
+        <h3>Meet and collaborate with likeminded students!</h3>  
 
         <div class="button-container">
         <a href="/signup"><button class="homeButton" type="button">Get started</button></a>
@@ -62,11 +62,93 @@ onMounted (() => {
 
 <style>
 .button {
-  font-size: 18px;
-  padding:5px;
+  font-size: 14px;
+  padding:2px;
   margin-top: -0%;
 /*     background: #2F728D;
 */    background: #008080;
+}
+
+@media (hover: hover) {
+ 
+ h1 a:hover {
+   border-bottom: 4px solid #2b91b9;
+ }
+}
+
+@media only screen and (max-width:1300px) {
+
+   .row {
+     width: 1000px;
+     margin: 8px 0;
+   }
+
+   .col-1 { 
+     padding: 0 12px 0 100px;
+     width: 100%;
+   }
+   .col-1::after {
+     position: absolute; 
+     right: 400px; 
+   }
+   .col-1 h2 {
+     
+     font-size: 40px;
+     width: 80%;
+   }
+   
+   .col-1 h3 {
+     font-size: 24px;
+     width: 80%;
+   }
+   .col-2 {
+     flex-basis: 100%;
+     justify-content: center;
+   } 
+  /*  .firstFloor {
+=======
+   .firstFloor {
+
+     padding-top: 10%;
+     width: 100%;
+     height: 0;
+     padding-top: 10%;
+     padding-bottom: 100%; 
+     position: relative
+   } */
+  /*  .firstFloorSVG {
+     position: absolute;
+     width: 100%;
+     height: 100%;
+   
+   } */
+  }
+@media only screen and (max-width:600px) { 
+ .svg {
+   display: none;
+ }
+
+
+ .col-1 {
+   margin-top: 40px;
+   padding: 0 4px 0 50px;
+ }
+ .col-1 h2 {
+   font-size: 30px;
+ }
+ .col-1 h4{
+   font-size: 7px;
+ }
+
+ .col-1 .homeButton {
+  font-size: small;
+  margin-left: 10px;
+  
+ }
+ .col-2 .homeImage {
+   width: 40%;
+   margin-top: 40px;
+ }
 }
 
 
