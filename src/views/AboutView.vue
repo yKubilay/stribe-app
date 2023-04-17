ad08ed39e9eb704ace6c197882b3587978576772
Kasper Iversen <kasperi@hiof.no>
2023-04-17 11:58:28 +0200

    explanatory text on swiper function

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -16,6 +16,7 @@
     </section>
       <br/>
       <h1 style="text-align: center;">Stribes' developers</h1>
+      <p>Swipe to see Stribe's developers</p>
   </div>
   
   <section class="container" id="about_container">
